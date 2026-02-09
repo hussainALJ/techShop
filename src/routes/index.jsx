cad25a1f8fb40f78034605e2a5ba07716481229b
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-02-09 15:22:14 +0300

    Create a Main Layout

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,18 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "../pages/Home";
+import MainLayout from "../layout/MainLayout";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <div>Home</div>
-    }
-])
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
-export const AppRouter = () => <RouterProvider router={router} />
+export const AppRouter = () => <RouterProvider router={router} />;
