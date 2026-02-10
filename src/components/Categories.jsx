4a46f57c3f4b79067783d72bae79044cbb21928c
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-02-11 00:33:49 +0300

    If mini true small render for categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,11 +2,14 @@ import { fetchCategories } from "../api/categoriesApi";
 import { useFetch } from "../api/useFetch";
 import CategoryCard from "./CategoryCard";
 
-export const Categories = (limit = null) => {
+export const Categories = ({limit = null, mini}) => {
   const { data, isLoading, error } = useFetch(fetchCategories);
 
   if (limit)
-    if (!isLoading && data.length > limit.limit) data.length = limit.limit;
+    if (!isLoading && data.length > limit) data.length = limit;
+
+  if (mini)
+    if (!isLoading) return data.map((category) => <div>{category.name}</div>);
 
   return isLoading ? (
     <div>Loading...</div>
