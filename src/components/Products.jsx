0b5540ca8c4626cc89c6089a08016d45721158df
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-02-10 17:21:14 +0300

    Clean Products.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,13 +1,9 @@
-import { Link } from "react-router-dom";
 import { fetchProducts } from "../api/productsApi";
 import { useFetch } from "../api/useFetch";
-import { useCart } from "../services/useCart";
-import { useEffect } from "react";
 import ProductCard from "./ProductCard";
 
 export const Products = (limit = null) => {
   const { data, isLoading, error } = useFetch(fetchProducts);
-  const { addToCart } = useCart();
 
   if (limit)
     if (!isLoading && data.length > limit.limit) data.length = limit.limit;
