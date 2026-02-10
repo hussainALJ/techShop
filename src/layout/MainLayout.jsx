b2a428f4c6eb75a56d95d1ca42916f4c836b9279
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-02-10 17:37:30 +0300

    Style the header

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -3,10 +3,16 @@ import { Link, NavLink, Outlet } from "react-router-dom";
 function MainLayout() {
   return (
     <>
-    <header>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/products">Store</NavLink>
-      <NavLink to="/cart" >Cart</NavLink>
+    <header className=" py-5">
+      <div className=" max-w-7xl mx-auto px-3 flex justify-between items-center ">
+        <div className=" text-3xl font-black ">Logo</div>
+        <div className=" flex justify-center gap-4 text-sm font-bold">
+          <NavLink to="/" >Home</NavLink>
+          <NavLink to="/products">Store</NavLink>
+          <NavLink to="/cart" >Cart</NavLink>
+        </div>
+        <div>Log in</div>
+      </div>
     </header>
     <main>
       <Outlet />
