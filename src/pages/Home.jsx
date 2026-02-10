fa29835f0d37b4acfda1166cc96794c92315b494
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-02-10 18:49:19 +0300

    Change Home page style for categories

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,12 @@ import { Products } from "../components/Products";
 function Home() {
   return (
     <div className=" container m-auto px-5 max-w-270 ">
-      <div className=" flex gap-4 w-full overflow-auto mt-25 ">
+      <div className=" flex gap-4 w-full overflow-auto mt-30 ">
         <Products limit= '7'/>
       </div>
-      <Categories />
+      <div className=" flex flex-col gap-4 w-full my-40">
+        <Categories limit= '5'/>
+      </div>
     </div>
   );
 }
