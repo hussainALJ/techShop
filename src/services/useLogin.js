06bac8a08d362ec2511bd5fa3b7f96e532e1eca2
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-02-12 17:13:07 +0300

    Delete useLogin

--- src/services/useLogin.js
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand";
-
-export const useLogin = create((set) => ({
-  email: "",
-  password: "",
-  login: (email, password) => set(() => ({email: email, password: password}))
-}));
