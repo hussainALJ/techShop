8d952262012934d62a4141d2b5ced94778ea5e1e
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-02-13 00:51:46 +0300

    Add debounce for the SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,19 @@
+import { useRef } from "react";
+
 function SearchBar({ search }) {
+  const timeoutRef = useRef(null);
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    timeoutRef.current = setTimeout(() => {
+      search(value);
+    }, 1000);
+  };
   return (
     <div className=" border-2 border-gray-600 border-solid rounded-xl p-2 md:w-100 ">
       <input
@@ -6,7 +21,7 @@ function SearchBar({ search }) {
         type="text"
         placeholder="Search..."
         onChange={(e) => {
-          search(e.target.value);
+          handleChange(e)
         }}
       />
     </div>
